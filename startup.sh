a500f67259d2ac45894de302b350ad6d56414338
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2025-03-12 10:16:22 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# Start Gunicorn
-gunicorn --bind=0.0.0.0:8000 app:app
+# Make the startup.sh script executable (just in case it's not already)
+chmod +x startup.sh
+
+# Install dependencies (make sure you have a requirements.txt in the same directory)
+pip install -r requirements.txt
+
+# Start the app using Gunicorn
+exec gunicorn -b 0.0.0.0:8000 app:app
